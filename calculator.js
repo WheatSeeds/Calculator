1e7e8f45a31f238090c39c6c81de44a38a60d83a
WheatSeeds <georg23kuz@gmail.com>
2024-02-19 00:32:41 +0600

    Version 0.0.4 / All basic func are ready

--- calculator.js
+++ calculator.js
@@ -1,43 +1,87 @@
 const numButton = document.querySelectorAll(".numButton")
 const signButton= document.querySelectorAll(".signButton");
 const equalButton = document.querySelector(".equalButton")
+const deleteButton = document.querySelector("#delete");
+const clearButton = document.querySelector("#clear");
 
 const output = document.querySelector("#output");
+const story = document.querySelector("#story");
+
 let a = "";
-let b = ""
+let b = "";
 let sign="";
 
+function updateScreen(){
+    (sign === "")
+        ?((a==="")
+             ?(output.innerHTML = 0)
+             :(output.innerHTML = a))
+         :((b==="")
+             ?(output.innerHTML = 0)
+             :(output.innerHTML = b));
+}
 
 
+
+clearButton.onclick = () =>{
+    a = "";
+    b = "";
+    sign="";
+    output.innerHTML = 0;
+    story.innerHTML = ".";
+}
+
+deleteButton.onclick = () =>{
+    if(sign===""){
+        a = a.slice(0, -1)
+        updateScreen()
+    }
+    else{
+        b = b.slice(0, -1)
+        updateScreen()
+    }
+}
 
 equalButton.onclick = () =>{
     switch(sign){
         case "+":
+            story.innerHTML = a + " " + sign + " " + b;
             output.innerHTML = add();
             break;
         case "-":
+            story.innerHTML = a + " " + sign + " " + b;
             output.innerHTML = subtract();
             break;
         case "/":
+            story.innerHTML = a + " " + sign + " " + b;
             output.innerHTML = divide();
             break;
         case "*":
+            story.innerHTML = a + " " + sign + " " + b;
             output.innerHTML = multiply();
             break;
 
     }
 }
+
 signButton.forEach(btn =>{
     btn.onclick = () => {
         sign = btn.textContent;
+        story.innerHTML = a + " " + sign + " " + b;
     }
 });
 
 numButton.forEach((btn) => {
-    btn.onclick = () =>
-        (sign === "")?
-            a += btn.textContent:
+    btn.onclick = () => {
+        if (sign === "") {
+            a += btn.textContent;
+            output.innerHTML = a;
+        } else {
             b += btn.textContent;
+            output.innerHTML = b;
+            story.innerHTML = a + " " + sign;
+        }
+    }
 });
 
 function add(){
